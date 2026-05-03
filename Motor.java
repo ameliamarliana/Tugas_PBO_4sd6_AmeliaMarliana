67eac994ce4d2296e7256f9936efdc2d12e430e3
ameliamarliana <amelimarliana07@gmail.com>
2026-05-03 23:20:56 +0700

    Add Motor class extending Kendaraan

---
+++ Motor.java
@@ -0,0 +1,20 @@
+// Class turunan Motor
+class Motor extends Kendaraan {
+    private String jenisMesin;
+
+    public Motor(String merk, int kecepatanMaksimal, String jenisMesin) {
+        super(merk, kecepatanMaksimal);
+        this.jenisMesin = jenisMesin;
+    }
+
+    @Override
+    void gerakkan() {
+        System.out.println("Motor " + merk + " melaju di jalan!");
+    }
+
+    @Override
+    void infoKendaraan() {
+        super.infoKendaraan();
+        System.out.println("Jenis Mesin: " + jenisMesin);
+    }
+}
